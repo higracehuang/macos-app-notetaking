80a4d5bd8bab6693b9be4f5212b167f00f763252
Grace Huang <higracehuang@gmail.com>
2022-10-24 14:32:47 -0500

    Initial Commit

---
+++ notetaking/notetakingApp.swift
@@ -0,0 +1,20 @@
+//
+//  notetakingApp.swift
+//  notetaking
+//
+//  Created by Le Huang on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct notetakingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
