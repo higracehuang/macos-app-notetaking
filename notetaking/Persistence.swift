eadfc5c9b7f0911c7657fbc7279e6687be8844f3
Grace Huang <higracehuang@gmail.com>
2022-10-28 22:00:43 -0500

    Removed the Item from core data

--- notetaking/Persistence.swift
+++ notetaking/Persistence.swift
@@ -14,8 +14,11 @@ struct PersistenceController {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
         for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let newNoteEntry = NoteEntry(context: viewContext)
+            newNoteEntry.createdAt = Date()
+            newNoteEntry.updatedAt = Date()
+            newNoteEntry.content = "(Content)"
+            newNoteEntry.title = "(title)"
         }
         do {
             try viewContext.save()
