561810d7b8d725d0dae55a0af6f0819c56b11f03
higracehuang <117108383+higracehuang@users.noreply.github.com>
2022-12-17 08:36:12 -0600

    Formatted with 2 tab instead of 4 tabs

--- notetaking/ContentView.swift
+++ notetaking/ContentView.swift
@@ -9,109 +9,109 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-    
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \NoteEntry.createdAt, ascending: true)],
-        animation: .default)
-    private var noteEntries: FetchedResults<NoteEntry>
-    
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(noteEntries) { noteEntry in
-                    NoteEntryView(noteEntry: noteEntry)
-                }
-            }
-            .toolbar {
-                ToolbarItem {
-                    Button(action: PersistenceController.shared.addNoteEntry) {
-                        Label("Add Note", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select a note")
+  @Environment(\.managedObjectContext) private var viewContext
+  
+  @FetchRequest(
+    sortDescriptors: [NSSortDescriptor(keyPath: \NoteEntry.createdAt, ascending: true)],
+    animation: .default)
+  private var noteEntries: FetchedResults<NoteEntry>
+  
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(noteEntries) { noteEntry in
+          NoteEntryView(noteEntry: noteEntry)
         }
+      }
+      .toolbar {
+        ToolbarItem {
+          Button(action: PersistenceController.shared.addNoteEntry) {
+            Label("Add Note", systemImage: "plus")
+          }
+        }
+      }
+      Text("Select a note")
     }
+  }
 }
 
 struct NoteEntryView: View {
-    @ObservedObject var noteEntry: NoteEntry
-    
-    @State private var titleInput: String = ""
-    @State private var contentInput: String = ""
-    
-    @State private var shouldShowDeleteButton: Bool = false
-    @State private var shouldPresentConfirm: Bool = false
-    
-    var body: some View {
-        if let title = noteEntry.title,
-           let updatedAt = noteEntry.updatedAt,
-           let content = noteEntry.content {
-            NavigationLink {
-                VStack {
-                    TextField("Title", text: $titleInput)
-                        .onAppear() {
-                            self.titleInput = title
-                        }
-                        .onChange(of: titleInput) { newTitle in
-                            PersistenceController.shared.updateNoteEntry(
-                                noteEntry: noteEntry,
-                                title: newTitle,
-                                content: contentInput)
-                        }
-                    
-                    TextEditor(text: $contentInput)
-                        .onAppear() {
-                            self.contentInput = content
-                        }
-                        .onChange(of: contentInput) { newContent in
-                            PersistenceController.shared.updateNoteEntry(
-                                noteEntry: noteEntry,
-                                title: titleInput,
-                                content: newContent)
-                        }
-                }
-                
-            } label: {
-                HStack {
-                    Text(title)
-                    Text(updatedAt, formatter: itemFormatter)
-                    Spacer()
-                    if shouldShowDeleteButton || shouldPresentConfirm {
-                        Button {
-                            shouldPresentConfirm = true
-                        } label: {
-                            Image(systemName: "minus.circle")
-                        }.buttonStyle(.plain)
-                            .confirmationDialog("delete_question",
-                                                isPresented: $shouldPresentConfirm) {
-                                Button("delete_confirm", role: .destructive) {
-                                    PersistenceController.shared.deleteNoteEntry(
-                                        noteEntry: noteEntry)
-                                }
-                            }
-                    }
-                }.onHover { isHover in
-                    shouldShowDeleteButton = isHover
-                }
-                
+  @ObservedObject var noteEntry: NoteEntry
+  
+  @State private var titleInput: String = ""
+  @State private var contentInput: String = ""
+  
+  @State private var shouldShowDeleteButton: Bool = false
+  @State private var shouldPresentConfirm: Bool = false
+  
+  var body: some View {
+    if let title = noteEntry.title,
+       let updatedAt = noteEntry.updatedAt,
+       let content = noteEntry.content {
+      NavigationLink {
+        VStack {
+          TextField("Title", text: $titleInput)
+            .onAppear() {
+              self.titleInput = title
+            }
+            .onChange(of: titleInput) { newTitle in
+              PersistenceController.shared.updateNoteEntry(
+                noteEntry: noteEntry,
+                title: newTitle,
+                content: contentInput)
+            }
+          
+          TextEditor(text: $contentInput)
+            .onAppear() {
+              self.contentInput = content
+            }
+            .onChange(of: contentInput) { newContent in
+              PersistenceController.shared.updateNoteEntry(
+                noteEntry: noteEntry,
+                title: titleInput,
+                content: newContent)
             }
         }
+        
+      } label: {
+        HStack {
+          Text(title)
+          Text(updatedAt, formatter: itemFormatter)
+          Spacer()
+          if shouldShowDeleteButton || shouldPresentConfirm {
+            Button {
+              shouldPresentConfirm = true
+            } label: {
+              Image(systemName: "minus.circle")
+            }.buttonStyle(.plain)
+              .confirmationDialog("delete_question",
+                                  isPresented: $shouldPresentConfirm) {
+                Button("delete_confirm", role: .destructive) {
+                  PersistenceController.shared.deleteNoteEntry(
+                    noteEntry: noteEntry)
+                }
+              }
+          }
+        }.onHover { isHover in
+          shouldShowDeleteButton = isHover
+        }
+        
+      }
     }
+  }
 }
 
 private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
+  let formatter = DateFormatter()
+  formatter.dateStyle = .short
+  formatter.timeStyle = .medium
+  return formatter
 }()
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(
-            \.managedObjectContext,
-             PersistenceController.preview.container.viewContext)
-    }
+  static var previews: some View {
+    ContentView().environment(
+      \.managedObjectContext,
+       PersistenceController.preview.container.viewContext)
+  }
 }
--- notetaking/Persistence.swift
+++ notetaking/Persistence.swift
@@ -8,73 +8,73 @@
 import CoreData
 
 struct PersistenceController {
-    static let shared = PersistenceController()
-    
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newNoteEntry = NoteEntry(context: viewContext)
-            newNoteEntry.createdAt = Date()
-            newNoteEntry.updatedAt = Date()
-            newNoteEntry.content = "(Content)"
-            newNoteEntry.title = "(title)"
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
-    
-    let container: NSPersistentContainer
-    
-    init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "notetaking")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url =
-            URL(fileURLWithPath: "/dev/null")
-        }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        container.viewContext.automaticallyMergesChangesFromParent = true
+  static let shared = PersistenceController()
+  
+  static var preview: PersistenceController = {
+    let result = PersistenceController(inMemory: true)
+    let viewContext = result.container.viewContext
+    for _ in 0..<10 {
+      let newNoteEntry = NoteEntry(context: viewContext)
+      newNoteEntry.createdAt = Date()
+      newNoteEntry.updatedAt = Date()
+      newNoteEntry.content = "(Content)"
+      newNoteEntry.title = "(title)"
     }
-    
-    func save() {
-        let viewContext = container.viewContext
-        do {
-            try viewContext.save()
-        } catch {
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
+    do {
+      try viewContext.save()
+    } catch {
+      let nsError = error as NSError
+      fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
     }
-    
-    func addNoteEntry() {
-        let newNoteEntry = NoteEntry(context: container.viewContext)
-        newNoteEntry.createdAt = Date()
-        newNoteEntry.updatedAt = Date()
-        newNoteEntry.title = "Untitled"
-        newNoteEntry.content = "TBD"
-        
-        save()
+    return result
+  }()
+  
+  let container: NSPersistentContainer
+  
+  init(inMemory: Bool = false) {
+    container = NSPersistentContainer(name: "notetaking")
+    if inMemory {
+      container.persistentStoreDescriptions.first!.url =
+      URL(fileURLWithPath: "/dev/null")
     }
-    
-    func updateNoteEntry(noteEntry: NoteEntry, title:String, content: String) {
-        noteEntry.content = content
-        noteEntry.title = title
-        noteEntry.updatedAt = Date()
-        
-        save()
+    container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    })
+    container.viewContext.automaticallyMergesChangesFromParent = true
+  }
+  
+  func save() {
+    let viewContext = container.viewContext
+    do {
+      try viewContext.save()
+    } catch {
+      let nsError = error as NSError
+      fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
     }
+  }
+  
+  func addNoteEntry() {
+    let newNoteEntry = NoteEntry(context: container.viewContext)
+    newNoteEntry.createdAt = Date()
+    newNoteEntry.updatedAt = Date()
+    newNoteEntry.title = "Untitled"
+    newNoteEntry.content = "TBD"
     
-    func deleteNoteEntry(noteEntry: NoteEntry) {
-        container.viewContext.delete(noteEntry)
-        save()
-    }
+    save()
+  }
+  
+  func updateNoteEntry(noteEntry: NoteEntry, title:String, content: String) {
+    noteEntry.content = content
+    noteEntry.title = title
+    noteEntry.updatedAt = Date()
+    
+    save()
+  }
+  
+  func deleteNoteEntry(noteEntry: NoteEntry) {
+    container.viewContext.delete(noteEntry)
+    save()
+  }
 }
